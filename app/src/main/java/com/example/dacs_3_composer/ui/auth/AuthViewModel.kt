1c6dfad50a24df6d749028f7cdd30fe4456e9bcf
Truong Chinh <truongchinh01777@gmail.com>
2026-06-19 01:08:29 +0700

    chỉnh sửa bug nhầm role

--- app/src/main/java/com/example/dacs_3_composer/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/dacs_3_composer/ui/auth/AuthViewModel.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
+
 class AuthViewModel : ViewModel() {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
